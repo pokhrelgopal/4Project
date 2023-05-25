e945307d4538817081c5b30bed20b36728234faa
kiararayanp <kiararayanp@gmail.com>
2023-05-25 13:14:27 +0545

    labs

--- client-signup.php
+++ client-signup.php
@@ -17,28 +17,47 @@ include 'connection.php';
     <form class="form">
         <div class="form-group">
             <label for="fullname">Full Name:</label>
-            <input type="text" id="fullname" placeholder="Enter your full name">
+            <input type="text" id="fullname" name="cname" placeholder="Enter your full name">
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" id="email" placeholder="Enter your email">
+            <input type="email" id="email" name="email" placeholder="Enter your email">
         </div>
         <div class="form-group">
             <label for="phone">Phone:</label>
-            <input type="text" id="phone" placeholder="Enter your phone number">
+            <input type="text" id="phone"name="phone" placeholder="Enter your phone number">
         </div>
         <div class="form-group">
             <label for="password">Password:</label>
-            <input type="password" id="password" placeholder="Enter your password">
+            <input type="password" id="password" name="password" placeholder="Enter your password">
         </div>
+        <!-- <div class="form-group">
+            <label for="password">Confirm Password:</label>
+            <input type="password" id="password" name="" placeholder="Re-enter your password">
+        </div> -->
         <div class="form-group">
-            <input class="btn btn-dark btn-bg" type="submit" value="Signup" style="width: 100%;">
+            <input class="btn btn-dark btn-bg" type="submit" value="Signup" name="signup" style="width: 100%;">
         </div>
         <div class="normal-text">
             <p>Already have an account?<a href="login.php">Login</a></p>
         </div>
     </form>
-
 </body>
-
 </html>
+<?php
+    // Check if form was submitted
+    if (isset($_POST['signup'])) {
+        // 
+        $cname = $_POST['cname'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $phone = $_POST['phone'];
+        $ins = "INSERT INTO client (cname,email,password,phone) VALUES ('$cname','$email','$password','$phone') ";
+        $result = $con->query($ins);
+        if ($result) {
+            header("Location:login.php");
+        } else {
+        }
+    }
+
+?>
